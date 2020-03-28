2b800ef3918c43451cd73565076207eae45209c7
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-03-28 15:52:57 +0100

    feat(project-preview): add hover effect

--- src/components/project-preview.js
+++ src/components/project-preview.js
@@ -5,13 +5,47 @@ import Img from 'gatsby-image'
 import styles from './project-preview.module.css'
 
 // todo(pinussilvestrus): add project preview images
-export default ({ project }) => (
-  <div className={styles.preview}>
-    <div className={styles.previewImage}>
+class ProjectPreview extends React.Component {
 
-    </div>
-    <div className={styles.previewContent}>
-      <span className={styles.projectTitle}>{project.title}.</span>{project.description}
-    </div>
-  </div>
-)
+  state = {
+    displayMore: false
+  }
+
+  handleMouseover = () => {
+    this.setState({
+      displayMore: true
+    })
+  }
+
+  handleMouseleave = () => {
+    this.setState({
+      displayMore: false
+    })
+  }
+
+  render() {
+    const { project } = this.props;
+    
+    const { displayMore } = this.state;
+
+    const moreStyle = {
+      display: displayMore ? 'block' : 'none'
+    };
+
+    return (
+      <div className={styles.preview}>
+        <div 
+          className={styles.previewImage}
+          onMouseOver={this.handleMouseover}
+          onMouseLeave={this.handleMouseleave} >
+            <div className={styles.more} style={moreStyle}><a>mehr erfahren</a></div>
+        </div>
+        <div className={styles.previewContent}>
+          <span className={styles.projectTitle}>{project.title}.</span>{project.description}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ProjectPreview;
