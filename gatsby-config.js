9c425607a039ed8587b4fd6986577b0f9e92e2c7
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-03-28 16:16:52 +0100

    feat(project): add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,34 @@
-require('dotenv').config({
+require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`
-})
+});
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   host: process.env.CONTENTFUL_HOST
-}
+};
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
-    'Contentful spaceId and the access token need to be provided.'
-  )
+    "Contentful spaceId and the access token need to be provided."
+  );
 }
 
 module.exports = {
   siteMetadata: {
-    title: 'Corinna Laabs Portfolio',
+    title: "Corinna Laabs Portfolio"
   },
-  pathPrefix: '/gatsby-contentful-starter',
+  pathPrefix: "/gatsby-contentful-starter",
   plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig
     }
-  ],
-}
+  ]
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import './base.css'
+import ReactGA from "react-ga";
 
+import "./base.css";
 
-import Container from './container'
+import Container from "./container";
 
 const __PREFIX_PATHS__ = process.env.__PREFIX_PATHS__;
 
 class Layout extends React.Component {
-  render() {
-    const { children, dimen } = this.props
 
-    return (
-      <Container dimen={dimen}>
-        {children}
-      </Container>
-    )
+  componentDidMount() {
+
+    // initialize Google Analytics on every page
+    ReactGA.initialize("UA-162105268-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    const { children, dimen } = this.props;
+
+    return <Container dimen={dimen}>{children}</Container>;
   }
 }
 
-export default Layout
+export default Layout;
